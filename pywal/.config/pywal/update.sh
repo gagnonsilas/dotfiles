01638dfaac4e405b157745f68903b5e72d18c0f6
gagnonsilas <gagnon.silas@gmail.com>
2023-12-04 17:43:20 -0500

    update pywal scripts

--- pywal/.config/pywal/update.sh
+++ pywal/.config/pywal/update.sh
@@ -1,7 +1,7 @@
 #! /sbin/zsh
 
 pywalfox update
-~/.config/pywal/scripts/pywal-obsidianmd/pywal-obsidianmd.sh ~/Obsidian
+~/.config/pywal/pywal-obsidianmd/pywal-obsidianmd.sh ~/Obsidian
 ~/.config/pywal/scripts/gtk/generate-theme.nu
 
 
