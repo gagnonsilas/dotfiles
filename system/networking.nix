e0cd1b2467cf4c6f3d5b51f7cd2fa8181e24f595
gagnonsilas <gagnon.silas@gmail.com>
2025-05-21 14:46:46 -0400

    wireguard config

--- system/networking.nix
+++ system/networking.nix
@@ -17,14 +17,29 @@
     ];
     firewall.enable = false;
 
-    nameservers = [
-      "8.8.8.8"
-      "8.8.4.4"
-    ];
+    # nameservers = [
+    #   "8.8.8.8"
+    #   "8.8.4.4"
+    # ];
 
     # Configure network proxy if necessary
     # networking.proxy.default = "http://user:password@proxy:port/";
     # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+    wg-quick.interfaces = {
+      serverworks = {
+        privateKeyFile = "/etc/wireguard/priv-key";
+        address = [ "10.0.0.4" ];
+        dns = [ "10.0.0.1" ];
+
+        peers = [ {
+          publicKey = "LDqLLPMJPuj1w2ea/JqEnDHcqeUxDqzgcu/rLAe8on4=";
+          endpoint = "75.130.94.103:14438";
+          persistentKeepalive = 25;
+          allowedIPs = [ "10.0.0.0/24" ];
+        } ];
+      };
+    };
   };
 
   services =  {
@@ -39,4 +54,6 @@
       enable = true;
     };
   };
+
+  
 }
